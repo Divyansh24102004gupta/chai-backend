d30fed26b58842740816773fc1ee63ae41d73f77
Divyanshu Gupta <divyanshug2410@gmail.com>
2024-04-04 12:36:19 +0530

    add changes

--- src/index.js
+++ src/index.js
@@ -10,6 +10,16 @@ dotenv.config({
 })
 connectDB()
 
+.than(()=>{
+    app.listen(process.env.PORT||8000 , ()=>{
+        console.log(`server is running at port ${process.env.PORT}`)
+    });
+})
+
+.catch((err) => {
+    console.log("mangoDb connection failed !!!  ",err);
+})
+
 
 
 
